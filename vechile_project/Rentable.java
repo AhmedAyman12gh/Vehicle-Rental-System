3dada837e623e7916ea2a6a0d62cdb37aea491a4
yassinharraz <yassin.harraz2013@gmail.com>
2025-12-13 20:18:48 +0200

    Initial commit

--- vechile_project/Rentable.java
+++ vechile_project/Rentable.java
@@ -1,3 +1,8 @@
-public interface Rentable {
-    
+public interface Rentable extends Comparable<Rentable> {
+    public boolean isAvailable();  // Check if the item is available for rent
+    public void rent(Customer customer, int days); // Rent the item to a customer for a specified number of days
+    public double getRentalPrice(int days); // Get the rental price for a specified number of days
+    public void returnItem(); // Return the rented item
+    public String getDescription(); // Get a description of the rentable item
+
 }
