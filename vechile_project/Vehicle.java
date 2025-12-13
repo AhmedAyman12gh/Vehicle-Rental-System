9691a7c2a99840d633c2d65c72e83c2acb48e96a
AHMED-ayman7 <ahmed.ayman.12gh@gmail.com>
2025-12-13 20:39:24 +0200

    vehicl update

--- vechile_project/Vehicle.java
+++ vechile_project/Vehicle.java
@@ -1,3 +1,36 @@
 public abstract class Vehicle {
+
+     private String vehicleId;      
+    private String brand;          
+    private String model;          
+    private int year;            
+    private double pricePerDay;  
+    private boolean isAvailable;   
+
+    public Vehicle(String vehicleId, String brand, String model, int year, double pricePerDay) {
+        this.vehicleId = vehicleId;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.pricePerDay = pricePerDay;
+        this.isAvailable = true;
     
 }
+    public abstract  String getVehicleId();
+    public abstract String getBrand();
+    public abstract String getModel();
+    public abstract int getYear();
+    public abstract double getPricePerDay();
+    public abstract boolean isAvailable();
+    public abstract void setAvailable(boolean available);
+    public abstract String getVehicleInfo();
+    @ Override
+    public String toString(){
+        return getVehicleInfo();
+    }
+
+
+}
+   
+
+    
