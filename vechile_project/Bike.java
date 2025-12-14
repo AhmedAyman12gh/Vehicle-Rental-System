03c8a24d5dc9edce62ac9b13faf1e586b9ad8786
Yousefbahig <2300837@eng.asu.edu.eg>
2025-12-14 19:30:16 +0200

    m

--- vechile_project/Bike.java
+++ vechile_project/Bike.java
@@ -1,3 +1,55 @@
-public class Bike extends Vehicle implements Rentable {
+public class Bike extends Vehicle {
+   
+    private String bikeType; // e.g., Minivan, Cargo Van, Passenger Van, etc.
+    private boolean Available;
+     
     
+    
+    
+    public Bike(String vehicleId, String brand, String model, int year, double pricePerDay, int quantity, String bikeType) {
+        super(vehicleId, brand, model, year, pricePerDay, quantity);
+        this.bikeType = bikeType;
+    }
+    
+    public String getBikeType() {
+        return bikeType;
+    }
+    @Override
+    public String getVehicleInfo() {
+        return "Car [ID=" + getVehicleId() + ", Brand=" + getBrand() + ", Model=" + getModel() + ", Year=" + getYear() +
+               ", PricePerDay=" + getPricePerDay() + ", CarType=" + getBikeType() + ", Available=" + isAvailable() + "]";
+    }
+
+
+
+
+    public void setAvailable(boolean available) {
+        this.Available = available;
+    }
+
+
+    public boolean isAvailable(){
+        return Available;
+    }
+    
+    
+    // // Check if the item is available for rent
+    // public void rent(Customer customer, int days){
+
+
+    // } // Rent the item to a customer for a specified number of days
+    // public double getRentalPrice(int days){
+       
+
+    // } // Get the rental price for a specified number of days
+    // public void returnItem(){
+
+    // } // Return the rented item
+    // public String getDescription(){
+       
+
+    // }// Get a description of the rentable item
 }
+
+   
+    
--- vechile_project/Van.java
+++ vechile_project/Van.java
@@ -1,16 +1,66 @@
-public class Van extends Vehicle implements Rentable{
+public class Van extends Vehicle {
    
-   
-   
+    private String vanType; // e.g., Minivan, Cargo Van, Passenger Van, etc.
+    private boolean Available;
+     
     
     
     
+    public Van(String vehicleId, String brand, String model, int year, double pricePerDay, int quantity, String vanType) {
+        super(vehicleId, brand, model, year, pricePerDay, quantity);
+        this.vanType = vanType;
+    }
     
-    
+    public String getVanType() {
+        return vanType;
+    }
+    @Override
+    public String getVehicleInfo() {
+        return "Car [ID=" + getVehicleId() + ", Brand=" + getBrand() + ", Model=" + getModel() + ", Year=" + getYear() +
+               ", PricePerDay=" + getPricePerDay() + ", CarType=" + getVanType() + ", Available=" + isAvailable() + "]";
+    }
 
-    
-   
-   
 
+
+
+    public void setAvailable(boolean available) {
+        this.Available = available;
+    }
+
+
+    public boolean isAvailable(){
+        return Available;
+    }
     
+    
+    // // Check if the item is available for rent
+    // public void rent(Customer customer, int days){
+
+
+    // } // Rent the item to a customer for a specified number of days
+    // public double getRentalPrice(int days){
+       
+
+    // } // Get the rental price for a specified number of days
+    // public void returnItem(){
+
+    // } // Return the rented item
+    // public String getDescription(){
+       
+
+    // }// Get a description of the rentable item
 }
+
+   
+    
+    
+    
+    
+    
+
+    
+   
+   
+
+    
+
